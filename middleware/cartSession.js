7a25e8e87154730d1f4b4597a0b6f7b63456d534
marrsDev <marrs2103@gmail.com>
2025-09-18 17:56:36 +0300

    Update cartSession.js

--- middleware/cartSession.js
+++ middleware/cartSession.js
@@ -2,17 +2,20 @@
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = (req, res, next) => {
-  // Get or create cart session ID
-  let cartId = req.cookies?.cartId;
+  // Check for header first, then cookie
+  let cartId = req.headers['x-cart-id'] || req.cookies?.cartId;
   
   if (!cartId) {
-    cartId = uuidv4();
-    res.cookie('cartId', cartId, {
-      maxAge: 365 * 24 * 60 * 60 * 1000, // 1 year
-      httpOnly: true,
-      sameSite: 'strict',
-      secure: process.env.NODE_ENV === 'production'
-    });
+    cartId = require('uuid').v4();
+    // Only set cookie if not session storage ID
+    if (!req.headers['x-cart-id']) {
+      res.cookie('cartId', cartId, {
+        maxAge: 365 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production'
+      });
+    }
   }
 
   req.cartId = cartId;
